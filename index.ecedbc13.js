c241c44d81f400f747a622774a71539c42d25286
AninotA <107799791+AninotA@users.noreply.github.com>
2023-04-03 10:38:10 +0000

    Deploying to gh-pages from @ AninotA/proste-okna-cz@6e90ff00fbafb28039cda9c54a8ef190d57884bc 🚀

---
+++ index.ecedbc13.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-main-open]"),openModalBtnsec:document.querySelector("[data-modal-main-opensec]"),closeModalBtn:document.querySelector("[data-modal-main-close]"),modal:document.querySelector("[data-main-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtnsec.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.ecedbc13.js.map
