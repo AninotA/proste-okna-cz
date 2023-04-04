35393c46e2f3d386792d6fc7774db2ad4b13d037
AninotA <107799791+AninotA@users.noreply.github.com>
2023-04-04 14:17:10 +0000

    Deploying to gh-pages from @ AninotA/proste-okna-cz@fa78319c2134baef81be2f10a4f36bf38af80d23 🚀

--- index.a78fd47d.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},effect:"flip",flipEffect:{slideShadows:!0,limitRotation:!0}});
-//# sourceMappingURL=index.a78fd47d.js.map
